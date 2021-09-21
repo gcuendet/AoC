69ba9e67f03de5563006031f5f69708e17462579
gcuendet <gabriel.cuendet@protonmail.ch>
2021-09-21 17:57:10 +0000

    Add second part for day 6

--- 2019/06.py
+++ 2019/06.py
@@ -23,17 +23,28 @@ def read_input(filepath):
 
 
 distance = {"COM": 0}
+inverted_connectivity = {}
 
 
 def orbits(k, _map):
     for conn in _map[k]:
         distance[conn] = distance[k] + 1
+        inverted_connectivity[conn] = k
         yield conn, distance[conn]
 
 
-if __name__ == "__main__":
-    args = parse_args()
-    _map = read_input(args.input)
+def path_to_com(k):
+    path  = []
+    _k = k
+    while _k != "COM":
+        next = inverted_connectivity[_k]
+        path.append(next)
+        _k=next
+    return path
+
+
+def part_1(input_path):
+    _map = read_input(input_path)
 
     number_of_orbits = 0
     list_of_name = ["COM"]
@@ -43,4 +54,24 @@ if __name__ == "__main__":
             number_of_orbits += o
             list_of_name.append(name)
         index += 1
-    print(number_of_orbits)
+    return number_of_orbits
+
+
+def jumps(path1, path2):
+    for el in path1:
+        if el in path2:
+            common_node = el
+            break
+    return path1.index(common_node) + path2.index(common_node)
+
+
+def part_2():
+    path1 = path_to_com("YOU")
+    path2 = path_to_com("SAN")
+    return jumps(path1, path2)    
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    print(part_1(args.input))
+    print(part_2())
